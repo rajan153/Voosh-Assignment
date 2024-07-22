9feaeaf823e4d912101accce59b909f090a0061b
Rajan <rajanmasih154@gmail.com>
2024-07-22 14:21:49 +0530

    chore: Add backend configuration and setup

---
+++ Backend/src/index.js
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+import connectDB from "./configs/Database.js";
+import { app } from "./app.js";
+
+dotenv.config({
+  path:"../env"
+});
+
+connectDB()
+  .then(() => {
+    try {
+      app.listen(process.env.PORT || 8000, () =>
+        console.log(`Server is running at ${process.env.PORT}`)
+      );
+    } catch (error) {
+      console.error("Server not running !!", error);
+    }
+  })
+  .catch((err) => console.log("MOGODB Connection failed!!", err));
