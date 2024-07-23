0cecac05e463266e887f14139f2c3a0f6774c54d
Rajan <rajanmasih154@gmail.com>
2024-07-24 01:47:16 +0530

    feat: Add Navbar component for website navigation

---
+++ Frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import logo from "/task.svg";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import ProfileDropDown from "./ProfileDropDown";
+import { AiOutlineMenu } from "react-icons/ai";
+
+function Navbar() {
+  const { token } = useSelector((state) => state.auth);
+  
+  return (
+    <div className="bg-slate-950 p-2 flex justify-between items-center">
+      <Link to={"/"} className="hover:cursor-pointer">
+        <img src={logo} alt="Navbar logo" width={40} loading="lazy" />
+      </Link>
+      {token === null && (
+        <div className="flex gap-4">
+          <Link
+            to={"/login"}
+            className="hidden md:block border border-[#13064b] bg-white px-[12px] py-[8px] text-[#13064b] rounded-md hover:bg-[#13064b] hover:text-white"
+          >
+            Login
+          </Link>
+          <Link
+            to={"/signup"}
+            className="hidden md:block border border-[#13064b] bg-white px-[12px] py-[8px] text-[#13064b] rounded-md hover:bg-[#13064b] hover:text-white"
+          >
+            Signup
+          </Link>
+        </div>
+      )}
+      {token !== null && <ProfileDropDown />}
+      <button className="mr-4 md:hidden">
+        <AiOutlineMenu fontSize={24} fill="#13064b" />
+      </button>
+    </div>
+  );
+}
+
+export default Navbar;
---
+++ Frontend/src/components/Navbar/ProfileDropDown.jsx
@@ -0,0 +1,74 @@
+import React, { useRef, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { AiOutlineCaretDown } from "react-icons/ai";
+import { VscDashboard, VscSignOut } from "react-icons/vsc";
+import useOnClickOutside from "../../hooks/useOnClickOutside";
+import { logout } from "../../services/AuthRoutes/AuthApi";
+import ConfirmationModal from "../Common/ConfirmationModal";
+
+function ProfileDropDown() {
+  const { registerData } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [open, setOpen] = useState(false);
+  const ref = useRef(null);
+  const [confirmationModal, setConfirmationModal] = useState(null);
+
+  useOnClickOutside(ref, () => setOpen(false));
+
+  if (!registerData) return null;
+
+  return (
+    <div className="relative cursor-pointer" onClick={() => setOpen(true)}>
+      <div className="flex items-center gap-x-1">
+        <img
+          src={registerData?.avatar}
+          alt={`profile-${registerData?.firstName}`}
+          className="aspect-square w-[30px] rounded-full object-cover"
+        />
+        <AiOutlineCaretDown className="text-sm text-richblack-100" />
+      </div>
+      {open && (
+        <div
+          onClick={(e) => e.stopPropagation()}
+          className="absolute top-[118%] right-0 z-[1000] divide-y-[1px] divide-richblack-700 overflow-hidden rounded-md border-[1px] border-richblack-700 bg-white"
+          ref={ref}
+        >
+          <Link to="/dashboard" onClick={() => setOpen(false)}>
+            <div className="flex w-full items-center gap-x-1 py-[10px] px-[12px] text-sm text-richblack-100 hover:bg-richblack-700 hover:text-richblack-25">
+              <VscDashboard className="text-lg" />
+              Dashboard
+            </div>
+          </Link>
+          <div className="flex flex-col">
+            <button
+              onClick={() =>
+                setConfirmationModal({
+                  text1: "Are you sure?",
+                  text2: "You will be logged out of your account.",
+                  btn1Text: "Logout",
+                  btn2Text: "Cancel",
+                  btn1Handler: () => dispatch(logout(navigate)),
+                  btn2Handler: () => setConfirmationModal(null),
+                })
+              }
+              className="flex w-full items-center gap-x-1 py-[10px] px-[12px] text-sm text-richblack-100 hover:bg-richblack-700 hover:text-richblack-25"
+            >
+              <div className="flex items-center gap-x-2">
+                <VscSignOut className="text-lg" />
+                <span>Logout</span>
+              </div>
+            </button>
+          </div>
+
+          {confirmationModal && (
+            <ConfirmationModal modalData={confirmationModal} />
+          )}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default ProfileDropDown;
