80e40fa1a443a0f01b23b6eaec50ab631a5a4bbe
Rajan <rajanmasih154@gmail.com>
2024-07-24 01:46:44 +0530

    refactor: Update main.jsx with routing, state management, and error handling improvements

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import { Provider } from "react-redux";
+import store from "./store/store.js"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <App />
+      <Toaster />
+    </BrowserRouter>
+  </React.StrictMode>
+  </Provider>
+);
