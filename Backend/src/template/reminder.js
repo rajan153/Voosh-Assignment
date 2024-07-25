3ed2f45869579e88e34d1060728e00644637a6ba
Rajan <rajanmasih154@gmail.com>
2024-07-25 20:46:36 +0530

    refactor: Add reminder email template

---
+++ Backend/src/template/reminder.js
@@ -0,0 +1,57 @@
+const reminderFunction = (firstName, lastName, taskId, title) => {
+  return `<!DOCTYPE html>
+      <html>
+      
+      <head>
+          <meta charset="UTF-8">
+          <title>OTP Verification Email</title>
+          <style>
+              body {
+                  background-color: #ffffff;
+                  font-family: Arial, sans-serif;
+                  font-size: 16px;
+                  line-height: 1.4;
+                  color: #333333;
+                  margin: 0;
+                  padding: 0;
+              }
+      
+              .container {
+                  max-width: 600px;
+                  margin: 0 auto;
+                  padding: 20px;
+                  text-align: center;
+              }
+      
+              .message {
+                  font-size: 18px;
+                  font-weight: bold;
+                  margin-bottom: 20px;
+              }
+      
+              .body {
+                  font-size: 16px;
+                  margin-bottom: 20px;
+              }
+      
+              .highlight {
+                  font-weight: bold;
+              }
+          </style>
+      
+      </head>
+      
+      <body>
+          <div class="container">
+              <div class="message">Reminder Email</div>
+              <div class="body">
+                  <p>Dear ${firstName} ${lastName},</p>
+                  <p class="highlight" class="highlight">Your Task ID: ${taskId}, Task Title: ${title} will be due after 1 hour.</p>
+              </div>
+          </div>
+      </body>
+      
+      </html>`;
+};
+
+export default reminderFunction;
