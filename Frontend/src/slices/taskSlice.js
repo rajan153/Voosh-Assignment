96063ab35fe98d2302f5336ce22dcd08c7451ad5
Rajan <rajanmasih154@gmail.com>
2024-07-25 19:44:21 +0530

    refactor: Update taskSlice.js to include new state properties and actions

--- Frontend/src/slices/taskSlice.js
+++ Frontend/src/slices/taskSlice.js
@@ -4,6 +4,10 @@ const initialState = {
   taskData: localStorage.getItem("taskData")
     ? JSON.parse(localStorage.getItem("taskData"))
     : null,
+  createdTodos: [],
+  processingTodos: [],
+  completedTodos: [],
+  searchResults: [],
 };
 
 const taskSlice = createSlice({
@@ -12,9 +16,50 @@ const taskSlice = createSlice({
   reducers: {
     setTaskData(state, action) {
       state.taskData = action.payload;
+
+      state.createdTodos = [];
+      state.processingTodos = [];
+      state.completedTodos = [];
+
+      action.payload.map((data, index) => {
+        switch (data.progressBar) {
+          case "Todos":
+            state.createdTodos.push(data);
+            break;
+          case "In-Progress":
+            state.processingTodos.push(data);
+            break;
+          case "Done":
+            state.completedTodos.push(data);
+            break;
+        }
+      });
+    },
+    setCreatedTodos(state, action) {
+      state.createdTodos = action.payload;
+    },
+    setProcessingTodos(state, action) {
+      state.processingTodos = action.payload;
+    },
+    setCompletedTodos(state, action) {
+      state.completedTodos = action.payload;
+    },
+    searchTasks(state, action) {
+      const query = action.payload.toLowerCase();
+      state.searchResults = state.taskData.filter(
+        (task) =>
+          task.title.toLowerCase().includes(query) ||
+          task.description.toLowerCase().includes(query)
+      );
     },
   },
 });
 
-export const { setTaskData } = taskSlice.actions;
+export const {
+  setTaskData,
+  setCompletedTodos,
+  setCreatedTodos,
+  setProcessingTodos,
+  searchTasks,
+} = taskSlice.actions;
 export default taskSlice.reducer;
