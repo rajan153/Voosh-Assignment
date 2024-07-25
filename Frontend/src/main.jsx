e8ed68464a08ada38e955c9f254d220e990611f6
Rajan <rajanmasih154@gmail.com>
2024-07-25 19:42:38 +0530

    feat: Integrate Auth0 for authentication in main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -5,15 +5,22 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import { Provider } from "react-redux";
-import store from "./store/store.js"
+import store from "./store/store.js";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-  <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Auth0Provider
+        domain="dev-4v3skqhmts30jc2a.us.auth0.com"
+        clientId="HrDMF2TYx6EOfuXOurEvuIvVbMoMRE6z"
+        authorizationParams={{
+          redirect_uri: window.location.origin,
+        }}
+      >
+        <App />
+      </Auth0Provider>
       <Toaster />
     </BrowserRouter>
-  </React.StrictMode>
   </Provider>
 );
